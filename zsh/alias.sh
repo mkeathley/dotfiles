c924c1e27e2df381b35710636e9b9712be2d3bb4
Mike Keathley <keathleymp@gmail.com>
2024-05-22 22:31:17 -0500

    Craziness with XDG configs

--- zsh/alias.sh
+++ zsh/alias.sh
@@ -5,4 +5,9 @@
 alias la='ls -lAh'
 
 # Dotfile command
+# Source: https://www.atlassian.com/git/tutorials/dotfiles
+# git init --bare $HOME/.cfg
+# alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+# config config --local status.showUntrackedFiles no
+# echo "alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'" >> $HOME/.bashrc
 alias dot='/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME/.config'
